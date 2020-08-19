b429433ebef15ec36f62924a4c96955ee11da8d3
Joseph cohen <jojocohen57@gmail.com>
2020-08-19 21:43:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ class App < Sinatra::Base
   end
 
   get '/set' do
-    # set the :foo key of the session hash equal to 'hello' here! 
-    session[:foo] = 'hello'
+    # set the :foo key of the session hash equal to 'hello' here!
+    session[:foo] = "hello"
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
@@ -33,14 +33,14 @@ class App < Sinatra::Base
   end
 
   get '/second_exercise' do
-    "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
+    "Your second lesson will be to set your session :user_id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:user_id] equal to 1.\nThen, navigate to the '/set_session' path"
   end
 
   get '/set_session' do
+    session[:user_id] = 1
     #set session id here
-    session[:user_id] == 1
     if session[:user_id] == 1
-      # "Session ID set. It's currently set to #{session[:id]}."
+      # "Session ID set. It's currently set to #{session[:user_id]}."
       redirect '/fetch_session_id'
     else
       "Session ID has not been set!"
@@ -48,11 +48,12 @@ class App < Sinatra::Base
   end
 
   get '/fetch_session_id' do
-    "You did it! session[:id] value: #{session[:user_id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
+    "You did it! session[:user_id] value: #{session[:user_id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
   end
 
   get '/logout' do
     session = {}
+
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
